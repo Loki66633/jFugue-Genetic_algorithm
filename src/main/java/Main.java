3e602d30a24d79abfbf9263ca9a76adbe8c0adc4
Loki <lovro.ivanovic@gmail.com>
2021-11-18 21:44:26 +0100

    Test

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import org.jfugue.player.Player;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Player player = new Player();
+        player.play("C D E F G A B A A A A A");
+    }
+}
