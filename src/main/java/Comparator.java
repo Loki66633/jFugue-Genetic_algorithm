106a4875581cd2ccaf339e5928fb84349341f6e6
Loki <lovro.ivanovic@gmail.com>
2021-11-21 21:23:26 +0100

    Test

---
+++ src/main/java/Comparator.java
@@ -0,0 +1,12 @@
+public class Comparator implements java.util.Comparator<Kromosom> {
+    @Override
+    public int compare(Kromosom o1, Kromosom o2) {
+        if (o1.getFf() > o2.getFf()) {
+            return 1;
+        } else if (o1.getFf() < o2.getFf()) {
+            return -1;
+        }
+        return 0;
+
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,49 @@
 import org.jfugue.player.Player;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Random;
 
 public class Main {
-    static final int FIELD_SIZE=7;
+
+    static char[]trazeneNote = new char[]{'C','D','E','F','G','A','B'};
+
+    static final int FIELD_SIZE=trazeneNote.length;
     static final int EVOLUCJE=1000;
     static final int POPULACIJA=2 ;
     static final int MUTATION_CHANCE=90;
     static final int MAX_AGE=200;
-    static Random random = new Random();
-    static final char[] sveNote = new char[]{'C','D','E','F','G','A','B'};
 
+
+    static final char[] sveNote = new char[]{'C','D','E','F','G','A','B'};
+    static Random random = new Random();
     static ArrayList<Kromosom> kromosoms = new ArrayList<>();
+
+
     public static void main(String[] args) {
 
         for(int i=0;i<POPULACIJA;i++){
             char[] a = new char[FIELD_SIZE];
             popuniRand(a);
+            System.out.print(calculateFF(a) + " ");
             printKromosom(a);
-            //kromosoms.add(new Kromosom(FIELD_SIZE,a,calculateFF(a)));
+            kromosoms.add(new Kromosom(a,FIELD_SIZE,calculateFF(a)));
         }
 
-        //Player player = new Player();
-        //player.play("C D E F G A B A A A A A");
+        int c=0;
+        boolean nemaRjesenja = true;
+
+        while(nemaRjesenja){
+            if(c > EVOLUCJE){
+                nemaRjesenja = false;
+                System.out.println("Riješenje nije nađeno!");
+            }
+            kromosoms.sort(new Comparator());
+            c++;
+        }
+
+        Player player = new Player();
+        //player.play("C D E F G A B");
     }
 
 
@@ -41,6 +61,16 @@ public class Main {
         }
         System.out.println();
 
+    }
+
+    public static int calculateFF(char[] note){
+        int ff=0;
+        for(int i=0;i<trazeneNote.length;i++){
+            if(note[i]==trazeneNote[i]){
+                ff++;
+            }
+        }
+        return ff;
     }
 
 
